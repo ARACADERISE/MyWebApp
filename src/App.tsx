cd16afce75a2b600b6089e861cbb733358f4140d
ARACADERISE <t.targetyt@gmail.com>
2021-06-02 18:34:48 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Header from './Files/Header'
 import './App.css';
 
+interface Layout {
+  headerTitle: string;
+  subTitle: string;
+  pages: {
+    firstPage: {
+      title: string | undefined;
+    }
+  }
+}
+
 const App = () => {
+  const [layout, setLayout] = useState<Layout | null>(null);
+
+  const onClick = useCallback(() => {
+    alert("Why you clicking on my name, huh?");
+
+    setLayout({
+      headerTitle: "Stop Clicking My Name",
+      subTitle: "MocaCDeveloper - Rust, C and Go developer",
+      pages: {
+        firstPage: {
+          title: layout?.pages.firstPage.title // same
+        }
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    fetch("/layout.json").then(
+      resp => resp.json()
+    ).then(data => {
+      setLayout(data);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <Header title="MocaCDeveloper" />
+      {/* Header */}
+      <Header title={layout?.headerTitle} subTitle={layout?.subTitle} onClick={onClick} />
+
+      {/* Welcome Page */}
+
+      {/* About Me Page */}
+
+      {/* My project */}
+
+      {/* Current Projects */}
+
+      {/* Collab with me */}
     </div>
   );
 }
--- src/Files/Header.tsx
+++ src/Files/Header.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { StringMappingType } from 'typescript'
 
 const Header: React.FunctionComponent<{
-    title: string
-}> = ({ title }) => {
+    title?: string;
+    onClick: () => void;
+    subTitle: string | undefined;
+}> = ({ title, onClick, subTitle }) => {
+    const githubOnclick = useCallback(() => {
+        window.open('https://github.com/ARACADERISE')
+    }, [])
+
     return (
-        <header>
-            <h1 className="border">{ title }</h1>
+        <header className="head">
+            {/* Left Buttons */}
+            <div className="left">
+                <button className="github" onClick={githubOnclick}>Github</button>
+                <button className="repl">Repl</button>
+            </div>
+            {/* Header including name, and sub header */}
+            <div className="border">
+                <h1 onClick={onClick}>{ title ? title : "MocaCDeveloper" }</h1>
+                <p>{subTitle !== "" ? subTitle : "Welcome!"}</p>
+            </div>
+            {/* Right buttons */}
+            <div className="right">
+                <button className="collab"><a style={{textDecoration: "none", color: "black"}} href="#">Collab</a></button>
+                <button className="seeProjects"><a style={{textDecoration: "none", color: "black"}} href="#">Projects </a></button>
+                <p style={{fontSize: 10}}>(Contact me at (402) 274-7838, or @ t.targetyt@gmail.com)</p>
+            </div>
         </header>
     )
 }
